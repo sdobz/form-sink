5935e194671fd72ac78230ed4f5a436624799249
Vincent Khougaz <vincent@khougaz.com>
2026-06-17 09:24:38 -0700

    Blank deno flake env

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "An endpoint to capture form submissions and email them";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          deno
+        ];
+      };
+    };
+}
